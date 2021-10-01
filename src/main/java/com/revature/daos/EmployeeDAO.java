c63a934286bc6bb601f6c6d2069e1d000abc7af2
unknown <zewley@gmail.com>
2021-10-01 13:07:16 -0400

    EmployeeDAO changes

--- src/main/java/com/revature/daos/EmployeeDAO.java
+++ src/main/java/com/revature/daos/EmployeeDAO.java
@@ -1,5 +1,50 @@
 package com.revature.daos;
 
+import com.revature.models.Employee;
+
 public class EmployeeDAO {
+
+	public void saveUser(Employee employee) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.save(employee);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	public boolean validate(String userName, String password) {
+
+		Transaction transaction = null;
+		Employee employee = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// get an user object
+			
+			String HQL = "FROM Employee U WHERE U.username = :userName";
+			employee = (Employee) session.createQuery(HQL).setParameter("userName", userName).uniqueResult();
+
+			if (employee != null && employee.getPassword().equals(password)) {
+				return true;
+			}
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		return false;
+	}
 
 }
