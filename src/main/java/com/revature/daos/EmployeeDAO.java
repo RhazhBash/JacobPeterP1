d823edbda554406dec88238ae9316c41fe3b74f4
unknown <zewley@gmail.com>
2021-10-01 14:06:30 -0400

    EmployeeDAO changed

--- src/main/java/com/revature/daos/EmployeeDAO.java
+++ src/main/java/com/revature/daos/EmployeeDAO.java
@@ -9,21 +9,18 @@ import com.revature.utils.HibernateUtil;
 
 public class EmployeeDAO {
 
-	public void saveUser(Employee employee) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
-			// save the student object
-			session.save(employee);
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
+	public void addEmployeee(Employee employee) {
+		
+		//open a Session object to establish a DB connection
+		Session ses = HibernateUtil.getSession(); //similar to opening a Connection with JDBC
+		
+		ses.save(employee); 
+		
+		HibernateUtil.closeSession();
+		
+		//This is the ENTIRE insert method - much cleaner than JDBC :)
+		//no try/catch? well, we aren't really writing any SQL that could go wrong. Simply using sessions methods
+		
 	}
 
 	public List<Employee> getAllEmployees() {
@@ -40,6 +37,17 @@ public class EmployeeDAO {
 
 	}
 	
+	public Employee findMovieById(int id){
+		
+		Session ses = HibernateUtil.getSession();
+		
+		Employee employee = ses.get(Employee.class, id);
+		
+		HibernateUtil.closeSession();
+		
+		return employee;
+		
+	}
 	
 	
 	public boolean validate(String userName, String password) {
