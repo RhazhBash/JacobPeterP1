92c995a8525c210f4435ac3be9e858537d6aea5f
Zewley <76058993+Zewley@users.noreply.github.com>
2021-10-03 21:13:40 -0400

    Update Ticket.java

--- src/main/java/com/revature/models/Ticket.java
+++ src/main/java/com/revature/models/Ticket.java
@@ -4,129 +4,241 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.hibernate.annotations.Table;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+
+
+@Entity
+@Table(name = "tickets")
 public class Ticket {
 	
-	private int TID; //Ticket ID
-	private int EID; //ID of the employee submitting the ticket
-	private String fname; //These should be self explanatory
-	private String lname;
-	private String type; //Lodging, travel, food, or other
-	private Double amount; //How much money was requested
-	private Double reimbursement; //How much was given to the employee
-	private String status; //Accepted, denied, pending, or documentation required
-	
-	//Pending is the default value for status, and a finance manager has to manually change it
-	
-	private String employeeDescription; //Employees must provide a brief description of why they're making the ticket
-	private String managerNotes; //For managers to give a reason for approving/denying a ticket
-	private String email; //Email address of the person making the ticket
-	private static final SimpleDateFormat timeSubmitted=new SimpleDateFormat("MM.dd.yyyy.HH.mm.ss"); //Timestamps for opening/closing a ticket
-	private static final SimpleDateFormat timeClosed=new SimpleDateFormat("MM.dd.yyyy.HH.mm.ss");
-	private String manager; //Name of the manager who handled the ticket, set upon first manager action taken
 	
 	
+	@Id //This makes this field the Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //This will make our PK serial
+	@Column(name = "ticket_id")
+	private int id; //Ticket ID
 	
-	public Ticket(int tID, int eID, String fname, String lname, String type, Double amount, Double reimbursement,
-			String status, String employeeDescription, String managerNotes, String email, String manager) {
-		super();
-		TID = tID;
-		EID = eID;
-		this.fname = fname;
-		this.lname = lname;
-		this.type = type;
-		this.amount = amount;
-		this.reimbursement = reimbursement;
-		this.status = status;
-		this.employeeDescription = employeeDescription;
-		this.managerNotes = managerNotes;
-		this.email = email;
-		this.manager = manager;
-	}
+	@Column(name= "ticket_amount")
+    private double Amount;
 	
+	//private static final SimpleDateFormat timeSubmitted=new SimpleDateFormat("MM.dd.yyyy.HH.mm.ss"); //Timestamps for opening/closing a ticket
+	//private static final SimpleDateFormat timeClosed=new SimpleDateFormat("MM.dd.yyyy.HH.mm.ss");
+    private Timestamp Submitted;
+    private Timestamp Resolved;
+    private String Description;
+    
+    
+    
+    //Assign an employee as the author of the ticket
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL) 
+	@JoinColumn(name = "employee_id")
+    private Employee Author;
+    
+	//Assign an employee as the manager of the ticket
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL) 
+	@JoinColumn(name = "employee_id")
+    private Employee Resolver;
+
+    //-1 refused, 0 not resolved, 1 acccepted
+    private int Status;
+    private String Type;
+    
+    
 	public Ticket() {
 		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Ticket(int id, double amount, Timestamp submitted, Timestamp resolved, String description, Employee author,
+			Employee resolver, int status, String type) {
+		super();
+		this.id = id;
+		Amount = amount;
+		Submitted = submitted;
+		Resolved = resolved;
+		Description = description;
+		Author = author;
+		Resolver = resolver;
+		Status = status;
+		Type = type;
 	}
 
 
 
-	public int getTID() {
-		return TID;
+	public Ticket(double amount, Timestamp submitted, Timestamp resolved, String description, Employee author,
+			Employee resolver, int status, String type) {
+		super();
+		Amount = amount;
+		Submitted = submitted;
+		Resolved = resolved;
+		Description = description;
+		Author = author;
+		Resolver = resolver;
+		Status = status;
+		Type = type;
 	}
-	public void setTID(int tID) {
-		TID = tID;
+
+	@Override
+	public String toString() {
+		return "Ticket [id=" + id + ", Amount=" + Amount + ", Submitted=" + Submitted + ", Resolved=" + Resolved
+				+ ", Description= " + Description +
+				", Author ID & Name: " + Author.getId() + " " + Author.getFirstName() + " " + Author.getLastName() + 
+				", Resolved by: " + Resolver.getId() + " " + Resolver.getFirstName() + " " + Resolver.getLastName() +
+				", Status= " + Status +
+				", Type= " + Type + "]";
 	}
-	public int getEID() {
-		return EID;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(Amount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((Author == null) ? 0 : Author.hashCode());
+		result = prime * result + ((Description == null) ? 0 : Description.hashCode());
+		result = prime * result + ((Resolved == null) ? 0 : Resolved.hashCode());
+		result = prime * result + ((Resolver == null) ? 0 : Resolver.hashCode());
+		result = prime * result + Status;
+		result = prime * result + ((Submitted == null) ? 0 : Submitted.hashCode());
+		result = prime * result + ((Type == null) ? 0 : Type.hashCode());
+		result = prime * result + id;
+		return result;
 	}
-	public void setEID(int eID) {
-		EID = eID;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ticket other = (Ticket) obj;
+		if (Double.doubleToLongBits(Amount) != Double.doubleToLongBits(other.Amount))
+			return false;
+		if (Author == null) {
+			if (other.Author != null)
+				return false;
+		} else if (!Author.equals(other.Author))
+			return false;
+		if (Description == null) {
+			if (other.Description != null)
+				return false;
+		} else if (!Description.equals(other.Description))
+			return false;
+		if (Resolved == null) {
+			if (other.Resolved != null)
+				return false;
+		} else if (!Resolved.equals(other.Resolved))
+			return false;
+		if (Resolver == null) {
+			if (other.Resolver != null)
+				return false;
+		} else if (!Resolver.equals(other.Resolver))
+			return false;
+		if (Status != other.Status)
+			return false;
+		if (Submitted == null) {
+			if (other.Submitted != null)
+				return false;
+		} else if (!Submitted.equals(other.Submitted))
+			return false;
+		if (Type == null) {
+			if (other.Type != null)
+				return false;
+		} else if (!Type.equals(other.Type))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
 	}
-	public String getFname() {
-		return fname;
+
+	public int getId() {
+		return id;
 	}
-	public void setFname(String fname) {
-		this.fname = fname;
+
+	public void setId(int id) {
+		this.id = id;
 	}
-	public String getLname() {
-		return lname;
+
+	public double getAmount() {
+		return Amount;
 	}
-	public void setLname(String lname) {
-		this.lname = lname;
+
+	public void setAmount(double amount) {
+		Amount = amount;
 	}
+
+	public Timestamp getSubmitted() {
+		return Submitted;
+	}
+
+	public void setSubmitted(Timestamp submitted) {
+		Submitted = submitted;
+	}
+
+	public Timestamp getResolved() {
+		return Resolved;
+	}
+
+	public void setResolved(Timestamp resolved) {
+		Resolved = resolved;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public Employee getAuthor() {
+		return Author;
+	}
+
+	public void setAuthor(Employee author) {
+		Author = author;
+	}
+
+	public Employee getResolver() {
+		return Resolver;
+	}
+
+	public void setResolver(Employee resolver) {
+		Resolver = resolver;
+	}
+
+	public int getStatus() {
+		return Status;
+	}
+
+	public void setStatus(int status) {
+		Status = status;
+	}
+
 	public String getType() {
-		return type;
+		return Type;
 	}
+
 	public void setType(String type) {
-		this.type = type;
+		Type = type;
 	}
-	public Double getAmount() {
-		return amount;
-	}
-	public void setAmount(Double amount) {
-		this.amount = amount;
-	}
-	public Double getReimbursement() {
-		return reimbursement;
-	}
-	public void setReimbursement(Double reimbursement) {
-		this.reimbursement = reimbursement;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public String getEmployeeDescription() {
-		return employeeDescription;
-	}
-	public void setEmployeeDescription(String employeeDescription) {
-		this.employeeDescription = employeeDescription;
-	}
-	public String getManagerNotes() {
-		return managerNotes;
-	}
-	public void setManagerNotes(String managerNotes) {
-		this.managerNotes = managerNotes;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getManager() {
-		return manager;
-	}
-	public void setManager(String manager) {
-		this.manager = manager;
-	}
-	public static SimpleDateFormat getTimesubmitted() {
-		return timeSubmitted;
-	}
-	public static SimpleDateFormat getTimeclosed() {
-		return timeClosed;
-	}
+	
+    
+    
+    
+    
+
 }
