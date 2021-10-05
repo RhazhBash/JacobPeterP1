ba795913a46528c384e9d34e098dddabe72a9270
Zewley <76058993+Zewley@users.noreply.github.com>
2021-10-05 16:09:33 -0400

    Update EmployeeDAO.java

--- src/main/java/com/revature/daos/EmployeeDAO.java
+++ src/main/java/com/revature/daos/EmployeeDAO.java
@@ -16,19 +16,14 @@ public class EmployeeDAO implements EmployeeDAOInterface{
 
 	public void newEmployee(Employee employee) {
 		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
-			// save the student object
-			session.save(employee);
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
+		Session session = HibernateUtil.getSession();
+		// start a transaction
+		transaction = session.beginTransaction();
+		// save the student object
+		session.save(employee);
+		// commit transaction
+		transaction.commit();
+		HibernateUtil.closeSession();
 	}
 
 	@SuppressWarnings("unchecked")      
